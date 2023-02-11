1f39485f6dd1cac05296a3fa1b64d829ed5f94ff
Ralf Zeilstra <ralfzeils@gmail.com>
2023-02-11 18:00:23 +0100

    Added world space ligth for mouse

--- Assets/Player/Scripts/MouseLight.cs
+++ Assets/Player/Scripts/MouseLight.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseWorld : MonoBehaviour
+public class MouseLight : MonoBehaviour
 {
     private void Update()
     {
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.Log( Physics.Raycast(ray));
+
+        Physics.Raycast(ray, out RaycastHit raycastHit);
+        
+        transform.position = raycastHit.point;
     }
 }

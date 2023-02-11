359920a07a41f3794fc465a587abe14b94f0c046
Ralf Zeilstra <ralfzeils@gmail.com>
2023-02-11 22:23:29 +0100

    Added mouse movement and rotation towards destination

--- Assets/Unit/Scripts/Unit.cs
+++ Assets/Unit/Scripts/Unit.cs
@@ -5,7 +5,12 @@ using UnityEngine.EventSystems;
 
 public class Unit : MonoBehaviour
 {
+    private const string START_WALKING_PARAMETER = "IsWalking";
+
+    [SerializeField] private Animator unitAnimator;
+
     [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float rotateSpeed = 20f;
     [SerializeField] private float stoppingDistance = 0.1f;
 
     private Vector3 targetPosistion;
@@ -22,11 +27,18 @@ public class Unit : MonoBehaviour
             Vector3 moveDirection = (targetPosistion - transform.position).normalized;
 
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
+
+            unitAnimator.SetBool(START_WALKING_PARAMETER, true);         
+        }
+        else
+        {
+            unitAnimator.SetBool(START_WALKING_PARAMETER, false);
         }
 
-        if (Input.GetKey(KeyCode.T))
+        if (Input.GetMouseButtonDown(0))
         {
-            Move(new Vector3(4, 0, 4));
+            Move(MouseWorld.GetPosition());
         }
     }
 }
